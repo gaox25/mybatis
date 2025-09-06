3b4e33909d04893d27ec42b464fed2136f0ca990
spitefire <60081445+gaox25@users.noreply.github.com>
2025-09-06 16:15:13 +0800

    学习如何获取Mapper代理对象以及进行CRUD操作

--- mybatis_quickstart/src/main/java/com/gaoxi/mapper/MonsterMapper.java
+++ mybatis_quickstart/src/main/java/com/gaoxi/mapper/MonsterMapper.java
@@ -2,6 +2,8 @@ package com.gaoxi.mapper;
 
 import com.gaoxi.entity.Monster;
 
+import java.util.List;
+
 /*
  1.这是一个接口，
  2.该接口用于声明操作monster表的方法
@@ -10,5 +12,17 @@ import com.gaoxi.entity.Monster;
 public interface MonsterMapper {
     //添加monster
     public void addMonster(Monster monster);
+
+    //根据id删除一个Monster
+    public void delMonster(Integer id);
+
+    //根据id更新Monster
+    public void updateMonster(Monster monster);
+
+    //根据id查询单个Monster
+    public Monster getMonsterById(Integer id);
+
+    //查询所有的Monster
+    public List<Monster> findAllMonster();
 
 }
--- mybatis_quickstart/src/test/java/com/gaoxi/mapper/MonsterMapperTest.java
+++ mybatis_quickstart/src/test/java/com/gaoxi/mapper/MonsterMapperTest.java
@@ -1,9 +1,13 @@
 package com.gaoxi.mapper;
 
+import com.gaoxi.entity.Monster;
 import com.gaoxi.util.MyBatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Date;
+import java.util.List;
 
 public class MonsterMapperTest {
     //属性
@@ -26,5 +30,78 @@ public class MonsterMapperTest {
         monsterMapper = sqlSession.getMapper(MonsterMapper.class);
         System.out.println("monsterMapper=" + monsterMapper.getClass());
     }
+    @Test
+    public void addMonster() {
+        for (int i = 0; i < 2; i++) {
+            Monster monster = new Monster();
+            monster.setAge(10 + i);
+            monster.setBirthday(new Date());
+            monster.setEmail("kate@qq.com");
+            monster.setGender(1);
+            monster.setName("kangroo-" + i);
+            monster.setSalary(1000 + i * 10);
+            monsterMapper.addMonster(monster);
+            System.out.println("添加对象" + monster);
+            System.out.println("添加到monster表中后，自增长的id = " + monster.getId());
+        }
+        //如果是增、删、改，需要提交事务，默认是不提交的
+        if (sqlSession != null) {
+            //提交事务
+            sqlSession.commit();
+            //释放连接到连接池
+            sqlSession.close();
+        }
+        System.out.println("保存成功");
+    }
 
+    @Test
+    public void delMonster() {
+        monsterMapper.delMonster(2);
+        if (sqlSession != null) {
+            sqlSession.commit();
+            sqlSession.close();
+        }
+        System.out.println("删除成功");
+    }
+
+    @Test
+    public void updateMonster() {
+        Monster monster = new Monster();
+        monster.setAge(50);
+        monster.setBirthday(new Date());
+        monster.setEmail("king@gmail.com");
+        monster.setGender(0);
+        monster.setName("mouse-01");
+        monster.setSalary(2000);
+        monster.setId(5);
+        monsterMapper.updateMonster(monster);
+        //提交事务
+        if (sqlSession != null) {
+            sqlSession.commit();
+            sqlSession.close();
+        }
+        System.out.println("更新成功");
+    }
+
+    @Test
+    public void getMonsterById() {
+        Monster monster = monsterMapper.getMonsterById(3);
+        System.out.println(monster);
+        if (sqlSession != null) {
+            //释放连接到连接池
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void findAllMonster() {
+        List<Monster> allMonster = monsterMapper.findAllMonster();
+        for (Monster monster : allMonster) {
+            System.out.println(monster);
+        }
+        //释放连接到连接池
+        if (sqlSession != null) {
+            sqlSession.close();
+        }
+    }
 }
